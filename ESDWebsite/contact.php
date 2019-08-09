7068a66f49eb0d57f7d0d0d6bf0595a69f8fadf8
brandonx21 <35933410+brandonx21@users.noreply.github.com>
2019-08-09 14:51:56 -0400

    ESD Website code

---
+++ ESDWebsite/contact.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+
+include 'action_page.php';
+
+?>
+  <head>
+  
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+   
+
+    <title>Advanced ESD Services+ | Services</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+	<link href="css/contactform.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="welcome.html">Advanced ESD Services+</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li><a href="welcome.html">Home</a></li>
+				<li><a href="experience.html">Experience</a></li>
+				 <li class="dropdown">
+				 
+                  <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="services.html#audits">Audits</a></li>
+                    <li><a href="services.html#engineering">Engineering</a></li>
+                    <li><a href="services.html#training">Training</a></li>
+                  </ul>
+				 
+                </li>
+				<li><a href="afilliations-and-friends.html">Affiliations</a></li>
+				<li class="active"><a href="contact.html">Contact</a></li>
+				<div class="navbar-brand-extended">
+				<i>"A Common Sense Approach"</i>
+				<div>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+    
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+ <div class="container marketing">
+		
+	
+	<div class="row">
+        <div class="col-lg-5">
+        </div>
+        <div class="col-lg-7">
+			<div class="push-down">
+        <h2><strong>Contact</strong></h2>
+			</div>
+        </div>
+	</div>
+
+	 <div class="container-form">
+	 
+  <form name="contact-form" method="post" action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="fname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lname" placeholder="Your last name..">
+	
+	<label for="email">Email</label>
+	<input type="text" id="email" name="email" placeholder="Enter a valid email address..">
+	
+	<label for="phone">Phone</label>
+	<input type="text" id="phone" name="phone" placeholder="Enter your phone number in format (xxx)-(xxx)-(xxxx)">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+	 <option value="usa">USA</option>
+      <option value="canada">Canada</option>
+    </select>
+
+	<label for="subject">Subject</label>
+	<input type="text" id="subject" name="subject" placeholder="Enter the subject of your email.">
+	
+    <label for="message">Message</label>
+    <textarea id="message" name="message" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit" name="submit">
+
+  </form>
+</div> 
+	
+	
+	
+	
+	
+</div>
+
+
+
+		
+     
+	 
+	
+		
+	  
+	  </body>
+	 
+      <div class="row featurette">
+        <div class="col-md-5">
+		<script type="text/javascript">
+	
+		</script>
+		
+        </div>
+        
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-5">
+
+        </div>
+       
+      </div>
+
+      
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="welcome.html">Back to top</a></p>
+        <p>&copy; 2018 Advanced ESD Services + &middot; <a href="contact.html">Contact Us</a></p>
+		
+		<div class="footer-social-media">
+			<a href="https://www.facebook.com/donn.bellmore"><img src="images/facebook.png" width="50px" height="50px"></a>
+			<a href="https://www.linkedin.com/in/donn-bellmore-a3bb2414/"><img src="images/linkedin.png" width="50px" height="50px"></a>
+		</div>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+	<script>
+	
+	
+	
+</html>
